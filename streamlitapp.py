000af19d4102600e26e67f28b330bcc42ecdd690
Praveensaich <97016531+Praveensaich@users.noreply.github.com>
2023-08-14 15:55:56 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,2 +1,8 @@
 import streamlit;
-streamlit.title(" My first project Streamlit ")
+streamlit.title(" My parents are new healthy dinner ");
+streamlit.header(" Breakfast Menu ");
+streamlit.text(" Idly ");
+streamlit.text(" Puri ");
+streamlit.text(" Uttampa ");
+streamlit.text(" Badgi ");
+
